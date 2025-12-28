e0d8c515ebcede43e5c62a3fe8f032c61b652629
ddbrown30 <ddbrown30@users.noreply.github.com>
2025-12-28 10:13:43 -0800

    Fixed deprecated function call and added restore of the expanded state

--- scripts/tcal.js
+++ scripts/tcal.js
@@ -24,18 +24,23 @@ export class TCAL {
             }
         }
 
-        // Save the current sidebar tab before importing
+        // Save the current sidebar state before importing
         const currentTab = ui.sidebar.tabGroups.primary;
+        const tabExpanded = ui.sidebar.expanded;
 
         updateData.folder = Utils.getSetting(SETTING_KEYS.transientFolder);
         updateData.flags = { tcal: {} };
         updateData.flags.tcal[FLAGS.isTransient] = true;
-        
+
         const importedActor = await game.actors.importFromCompendium(game.packs.get(actor.pack), actor.id, updateData);
 
-        // Restore the previous sidebar tab if it changed
+        // Restore the previous sidebar state if it changed
         if (currentTab && ui.sidebar.tabGroups.primary !== currentTab) {
-            ui.sidebar.activateTab(currentTab);
+            ui.sidebar.changeTab(currentTab, "primary");
+        }
+
+        if (ui.sidebar.expanded != tabExpanded) {
+            ui.sidebar.toggleExpanded(tabExpanded);
         }
 
         return importedActor;
@@ -44,4 +49,4 @@ export class TCAL {
     static isTransientActor(actor) {
         return actor ? Utils.getModuleFlag(actor, FLAGS.isTransient) : false;
     }
-} 
+}
